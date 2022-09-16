0685207ead2d014d1a81656e424d9e76505eb361
jyvillad <109568289+jvillad@users.noreply.github.com>
2022-09-16 13:57:24 +1000

    NavBar
    Adding responsive navigation bar and removed unnecessary line of code in HTML and CSS.

---
+++ site.js
@@ -0,0 +1,10 @@
+
+
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', function(){
+    nav.classList.toggle('nav-active');
+    
+});
