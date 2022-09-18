8ba434e0c8bac9a8b732d8f35eb177b16f11da39
jvillad <109568289+jvillad@users.noreply.github.com>
2022-09-18 18:33:35 +1000

    Update site v1

--- site.js
+++ site.js
@@ -1,10 +1,21 @@
 
-
-
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
 burger.addEventListener('click', function(){
     nav.classList.toggle('nav-active');
-    
 });
+
+const onClick = document.querySelectorAll('.nav-links a');
+console.log(onClick);
+
+onClick.forEach(element => {
+    element.addEventListener('click', () => {
+        nav.classList.remove('nav-active')
+    })
+});
+
+
+ 
+
+
